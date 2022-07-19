ec9923aaac33fa76555fdbdc51638ee003a4a6f8
arpit-ayeeee <arpitjaiswal282000@gmail.com>
2022-07-20 00:52:13 +0530

    Message snippet in inbox achieved, backend route completed, payload in frontend recieved

--- client/src/Components/inbox.js
+++ client/src/Components/inbox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios';
 import { useState} from 'react';
 import {useNavigate} from 'react-router-dom'
@@ -6,14 +6,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar,faArrowUp,faUsersViewfinder } from '@fortawesome/free-solid-svg-icons';
 import { NavLink } from 'react-router-dom';
 import Navbar from '../Components/navbar';
+
 const inbox = () => {
-
-
-    const [InboxMailList, setInboxMailList] = useState([{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'24may',sender:'roy@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'24may',sender:'roy@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'24may',sender:'roy@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'24may',sender:'roy@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com'},{date:'24may',sender:'roy@yahoo.com'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'22may',sender:'kevin@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'23may',sender:'john@dmail.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'24may',sender:'roy@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'},{date:'25may',sender:'will@yahoo.com',body:'asf Afdsag agsags gsgas sfgfs sfgs dsff sfgs  sfg gs  gsdfg sefg sd sg s s gsdg sgsd sg sg sg sd'}]);
-
     let history = useNavigate();
+   
+    const [snippetList, setSnippetList] = useState([]);
+
+    useEffect(()=>{
+        async function fetchSnippet() {
+            const currentAccess=localStorage.getItem('accessToken')
+            const data= await axios.post("/getSnippet",{currentAccess});
+            console.log(data.data);
+            setSnippetList(data.data);
+          }
+        fetchSnippet();
+    },0)
+
     const Viewpage= async()=>{
-        const data=await axios.post("/mails")
+        // const data=await axios.post("/getSnippet",{currentAccess});
+        // console.log(data);
         history('/view')
     }
     
@@ -32,19 +43,17 @@ const inbox = () => {
             </div>
             <div class="p-2 bg-warning" ></div>
             <ul class="list-group">
-                {InboxMailList.map((mail, index)=>{
+                {snippetList.map((mail, index)=>{
                     
                     return <li class="list-group-item" key= { index } >
                         <div>
                             <div class="row">
                                 <div class="col">
-                                    <h5>{mail.sender} </h5>
+                                    <h5>{mail.messageFrom[0].value} {mail.messageDate[0].value} </h5>
                                    
-                                    <div class="text-warning">{mail.date} </div>
+                                    <div class="text-warning">{mail.snippet}</div>
                                 </div>
-                                <div class="col"></div>
-                                <div class="col"></div>
-                                <button  class="btn  btn-sm col-1 buttom-customise" onClick={Viewpage} ><FontAwesomeIcon icon={faUsersViewfinder} /></button> 
+                                <button  class="btn  btn-sm col-1 buttom-customise" onClick={Viewpage}><FontAwesomeIcon icon={faUsersViewfinder} /></button> 
                                 <button class="btn  btn-sm col-1  buttom-customise"><FontAwesomeIcon icon={faStar} /></button>
                                 <button class="btn  btn-sm col-1 buttom-customise"><FontAwesomeIcon icon={faArrowUp} /></button> 
                             </div>
